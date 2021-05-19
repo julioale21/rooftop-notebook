7356e25686031b1caf666a23172ed3f64e930a45
Julio Romero <julio_ale21@hotmail.com>
2021-05-19 17:27:15 -0300

    chore: added Author entity

---
+++ Author.ts
@@ -0,0 +1,43 @@
+export class Author {
+  private id: String;
+  private firstName: String;
+  private lastName: String;
+  private age: Number;
+  private nacionality: String;
+
+  constructor() {
+    this.id = String(new Date());
+  }
+
+  public getFirstName(): String {
+      return this.firstName;
+  }
+
+  public setFirstName(firstName: String): void {
+      this.firstName = firstName;
+  }
+
+  public getLastName(): String {
+      return this.lastName;
+  }
+
+  public setLastName(lastName: String): void {
+      this.lastName = lastName;
+  }
+
+  public getAge(): Number {
+      return this.age;
+  }
+
+  public setAge(age: Number): void {
+      this.age = age;
+  }
+
+  public getNacionality(): String {
+      return this.nacionality;
+  }
+
+  public setNacionality(nacionality: String): void {
+      this.nacionality = nacionality;
+  }
+}
